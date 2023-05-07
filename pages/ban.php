25064e2a6529de228b7c6690a10a3824c13ba3dc
arkayzdev <franck.zhuang@hotmail.com>
2023-05-07 13:50:08 +0200

    Commit

--- pages/ban.php
+++ pages/ban.php
@@ -1,11 +1,16 @@
 <?php 
 session_start();
-$ban = (isset($_SESSION['status']) && $_SESSION['status'] == 'ban') ? true : false;
-if(!$ban){ 
-    header('location: https://flutters.ovh');
-    exit();
-}
-?>
+include($_SERVER['DOCUMENT_ROOT'] . '/pages/connect_db.php');
+if (isset($_SESSION['email'])) {
+    $email = $_SESSION['email'];
+    $q = "SELECT status FROM USERS WHERE email = '$email'";
+    $req = $bdd->query($q);
+    $result = $req->fetch(PDO::FETCH_ASSOC);
+    if ($result['status'] == 'none') {
+        header('location: https://flutters.ovh');
+        exit;
+    } 
+} ?>
 
 <!DOCTYPE html>
 <html >
